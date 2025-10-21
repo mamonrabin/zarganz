9bc40d4f40e44c526d78a8d31d2fca96a2039031
mamonrabin <almamon757@gmail.com>
2025-10-21 23:31:29 +0600

    push

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
 const Loading = () => {
   return (
-    <div className="w-full h-screen bg-[red] flex justify-center items-center">
+    <div className="w-full h-screen bg-[#AC0000] flex justify-center items-center">
       <div className="relative w-[120px] h-[120px]">
         {/* Circular spinner */}
-        <div className="absolute inset-0 rounded-full border-4 border-t-primary border-r-transparent border-b-transparent border-l-transparent animate-spin"></div>
+        <div className="absolute inset-0 rounded-full border-4 border-t-[#fff] border-r-transparent border-b-transparent border-l-transparent animate-spin"></div>
       </div>
     </div>
   );
